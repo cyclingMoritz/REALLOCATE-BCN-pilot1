d50984ffec913af0d3b993c1baac7eb20d50f0dc
“cyclingMoritz” <luca.liebscht@estudiantat.upc.edu>
2025-12-19 12:50:48 +0100

    Change config

--- js/layers_accidents.js
+++ js/layers_accidents.js
@@ -8,22 +8,19 @@ var mainAttribution = "Map by BSC - DataViz Team";
 var layers = [
   {
     name: "Accidents Points",
-    sourceLayerName: "accidents", //source-layer & source
-    attribution:
-      "Accessibility Data <a href='https://opendata-ajuntament.barcelona.cat/data/ca/dataset/accessibilitat-via-publica'>©Ajuntament de Barcelona</a> (<a href='https://creativecommons.org/licenses/by/4.0/deed.ca'>CC BY 4.0</a>)",
+    sourceLayerName: "accidents2024", //source-layer & source
+    attribution:  "",
     sourceType: "geojson",
     layerType: "circle",
     symbolization: {      
       "circle-stroke-width": 1,
       "circle-stroke-color": "#333",
       "circle-color": [
-        "match",
-        ["get", "Type"],   // property name in your GeoJSON
-        "Obstacles", "hsl(211, 25%, 61%)",
-        "Unevenness", "hsl(120, 40%, 55%)",
-        "Width", "hsl(30, 70%, 60%)",
-        /* default color */ "hsl(0, 0%, 70%)"
-      ],
+              "match",
+              ["get", "Evaluation"],   // property name in your GeoJSON
+              "Accessible", "#A6D96A",
+              /* default color */ "hsl(0, 0%, 70%)"
+            ],
       "circle-radius": [
         "interpolate",
         ["linear"],
@@ -37,12 +34,12 @@ var layers = [
       id: "legend-a",
       class: "legend",
       items: [
-        {
-          styleHeight: "10px",
-          display: "inline-block",
-          backgroundColor: "hsl(211, 25%, 61%)",
-          range: ["Points"],
-        },
+    {
+      styleHeight: "12px",
+      display: "inline-block",
+      backgroundColor: "#A6D96A",
+      range: ["Accessible"]
+    }
       ],
     },
     states: {
